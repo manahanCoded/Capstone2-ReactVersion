6a7f0100fd11b067e6807ec79347b69c4b5c9c57
unknown <manahandionell1@gmail.com>
2025-02-10 07:57:49 +0800

    connection of client and server v12

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://capstone2-react-version-7wzg.vercel.app",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
