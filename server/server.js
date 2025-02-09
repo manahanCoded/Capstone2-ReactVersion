da200e01fbeb42ec7a8684621739137c1da97016
unknown <manahandionell1@gmail.com>
2025-02-09 15:06:16 +0800

    fix server.js  file for server deployment v7

--- server/server.js
+++ server/server.js
@@ -6,20 +6,22 @@ import cors from "cors";
 import env from "dotenv";
 import cookieParser from "cookie-parser";
 
-
+//ROUTES
 import User_Routes from "./src/Routes/User_Routes.mjs";
 import Job_Routes from "./src/Routes/Job_Routes.mjs";
-import Module_Routes from "./src/Routes/Module_Routes.mjs";
-import Announcement_Routes from "./src/Routes/Announcement_Routes.mjs";
-import Dashboard_Routes from "./src/Routes/Dashboard_Routes.mjs";
-import Mail_Routes from "./src/Routes/Mail_Routes.mjs";
-import QA_Routes from "./src/Routes/QA_Routes.mjs";
+import Module_Routes from "./src/Routes/Module_Routes.mjs"
+import Announcement_Routes from "./src/Routes/Announcement_Routes.mjs"
+import Dashboard_Routes from "./src/Routes/Dashboard_Routes.mjs"
+import Mail_Routes from "./src/Routes/Mail_Routes.mjs"
+import QA_Routes from "./src/Routes/QA_Routes.mjs"
 
 const app = express();
-env.config();
+
 
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+env.config();
 
 app.use(
   session({
@@ -42,18 +44,18 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Backend is working!");
-});
-
-
 app.use("/api/user", User_Routes);
 app.use("/api/job", Job_Routes);
-app.use("/api/module", Module_Routes);
-app.use("/api/announcement", Announcement_Routes);
-app.use("/api/dashboard", Dashboard_Routes);
-app.use("/api/mail", Mail_Routes);
-app.use("/api/question-answer", QA_Routes);
+app.use("/api/module", Module_Routes)
+app.use("/api/announcement", Announcement_Routes)
+app.use("/api/dashboard", Dashboard_Routes)
+app.use("/api/mail", Mail_Routes)
+app.use("/api/question-answer", QA_Routes)
 
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 export default app;
