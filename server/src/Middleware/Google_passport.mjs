ebd0df36b4cf6f18e1643e6ae37a1a70a72e8d56
unknown <manahandionell1@gmail.com>
2025-02-10 11:20:00 +0800

    connection of client and server v21

--- server/src/Middleware/Google_passport.mjs
+++ server/src/Middleware/Google_passport.mjs
@@ -10,7 +10,7 @@ env.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.Google_ID, 
     clientSecret: process.env.Client_Secret,
-    callbackURL: 'https://capstone2-react-version-7wzg.vercel.app/api/user/auth/google/callback',
+    callbackURL: 'https://capstone2-react-version-kqvu.vercel.app/api/user/auth/google/callback',
     passReqToCallback: true
   },
   async (request, accessToken, refreshToken, profile, done) => {
